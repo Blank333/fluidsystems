b349b9a5ccdc958c5496df9e69123070e46d66c7
Anirudh <60171262+Blank333@users.noreply.github.com>
2024-02-28 14:45:29 +0530

    Fixed image paths

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,10 +1,13 @@
 import "./Content.css";
+import award from "../assets/1.png";
+import awardRec from "../assets/2.png";
+import achieve from "../assets/3.png";
 
 function Content() {
   return (
     <main>
       <div className='award'>
-        <img src='/src/assets/1.png' alt='award' className='awardImg' />
+        <img src={award} alt='award' className='awardImg' />
         <div className='awardDesc'>
           <p>
             <strong>C.R.I. PUMPS WINS THE NATIONAL ENERGY CONSERVATION AWARD 2018 for the 4th time.</strong>
@@ -20,7 +23,7 @@ function Content() {
               enabled control panel.
             </li>
           </ul>
-          <img src='/src/assets/2.png' alt='Receiving award' className='awardImgRec' />
+          <img src={awardRec} alt='Receiving award' className='awardImgRec' />
           <p className='awardText'>
             Government of India has awarded the <strong>&quot;National Energy Conservation Award 2018&quot;</strong>.
             Mr. G. Selvaraj, Joint Managing Director of C.R.I. Group received the award from Smt. Sumitra Mahajan,
@@ -34,7 +37,7 @@ function Content() {
           INSTALLED OVER 10 LAKHS STAR RATED PUMPSETS ACROSS THE COUNTRY RESULTING IN A CUMULATIVE SAVING OF MORE THAN
           9,000 MILLION UNITS OF POWER FOR THE NATION.
         </p>
-        <img src='/src/assets/3.png' alt='Fluid components' className='achieveImg' />
+        <img src={achieve} alt='Fluid components' className='achieveImg' />
         <p>Valves - Pumps - Pipes - IoT Drives & Controllers - Wires & Cables - Solar Systems - Motors</p>
       </div>
     </main>
